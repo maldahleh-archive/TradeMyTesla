b93eaef90ed982ff18147fef301c08a8fa99f77c
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-07 23:36:01 -0500

    Implement data and model

---
+++ MLModel/convert_cars.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LinearRegression
+import pandas
+import coremltools
+
+data = pandas.read_csv("cars.csv")
+
+model = LinearRegression()
+model.fit(data[["model", "premium", "mileage", "condition"]], data["price"])
+
+coreml_model = coremltools.converters.sklearn.convert(model, ["model", "premium", "mileage", "condition"], "price")
+coreml_model.author = "Mohammed Al-Dahleh"
+coreml_model.license = "CC0"
+coreml_model.short_description = "Predicts the trade-in price of a Tesla car."
+
+coreml_model.save("Cars.mlmodel")
